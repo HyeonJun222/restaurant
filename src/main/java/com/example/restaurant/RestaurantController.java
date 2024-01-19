de8aa89721749648f4d6f1b49ee087a2d30eb47b
HyeonJun222 <151013766+HyeonJun222@users.noreply.github.com>
2024-01-19 10:13:25 +0900

    Restaurant CRUD

--- src/main/java/com/example/restaurant/RestaurantController.java
+++ src/main/java/com/example/restaurant/RestaurantController.java
@@ -6,7 +6,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+/*
+    POST    /restaurant -> 새로운 레스토랑 데이터 (CREATE)
+    GET     /restaurant -> 모든 레스토랑 데이터 (READ ALL)
+    GET     /restaurant/{id} -> id 를 가진 레스토랑 데이터 (READ)
+    PUT     /restaurant/{id} -> id 의 위치에 전달될 레스토랑 데이터 (UPDATE)
+    DELETE  /restaurant/{id} -> id 의 위치에 삭제할 레스토랑 데이터 (DELETE)
+ */
 @Slf4j
 @RestController
 @RequestMapping("/restaurant")
@@ -19,12 +25,12 @@ public class RestaurantController {
             @RequestBody
             RestaurantDto dto
     ) {
-        throw new RuntimeException("not implemented");
+        return service.create(dto);
     }
 
     @GetMapping
     public List<RestaurantDto> readAll() {
-        throw new RuntimeException("not implemented");
+        return service.readAll();
     }
 
     @GetMapping("/{id}")
@@ -32,7 +38,7 @@ public class RestaurantController {
             @PathVariable("id")
             Long id
     ) {
-        throw new RuntimeException("not implemented");
+        return service.read(id);
     }
 
     @PutMapping("/{id}")
@@ -42,7 +48,7 @@ public class RestaurantController {
             @RequestBody
             RestaurantDto dto
     ) {
-        throw new RuntimeException("not implemented");
+        return service.update(id, dto);
     }
 
     @DeleteMapping("/{id}")
@@ -50,6 +56,6 @@ public class RestaurantController {
             @PathVariable("id")
             Long id
     ) {
-        throw new RuntimeException("not implemented");
+        service.delete(id);
     }
 }
--- src/main/java/com/example/restaurant/RestaurantService.java
+++ src/main/java/com/example/restaurant/RestaurantService.java
@@ -11,6 +11,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -19,22 +20,65 @@ public class RestaurantService {
     private final RestaurantRepository repository;
 
     public RestaurantDto create(RestaurantDto dto) {
-        throw new RuntimeException("not implemented");
+        Restaurant restaurant = new Restaurant(
+                dto.getName(),
+                dto.getCategory(),
+                dto.getOpenHour(),
+                dto.getCloseHour()
+        );
+        return RestaurantDto.fromEntity(repository.save(new Restaurant()));
     }
 
     public List<RestaurantDto> readAll() {
-        throw new RuntimeException("not implemented");
+        /*
+        List<RestaurantDto> restaurantDtoList = new ArrayList<>();
+        // 레스토랑 모든 정보를 가져옴
+        List<Restaurant> restaurantList = repository.findAll();
+        for (Restaurant entity: restaurantList){
+            restaurantDtoList.add(RestaurantDto.fromEntity((entity)));
+        }
+        return restaurantDtoList;
+        */
+//            STREAM API 활용
+            return repository
+                    .findAll()
+                    .stream()
+//                    .map(entity -> RestaurantDto.fromEntity(entity))  // 참조로 바꾸라 함
+                    .map(RestaurantDto::fromEntity)
+                    .collect(Collectors.toList());
+
     }
 
     public RestaurantDto read(Long id) {
-        throw new RuntimeException("not implemented");
+        /*
+        Optional<Restaurant> optionalRestaurant = repository.findById(id);
+        if (optionalRestaurant.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return RestaurantDto.fromEntity(optionalRestaurant.get());
+        */
+        // Optional.map(), Optional.orElseThrow()
+        return repository.findById(id)
+                .map(RestaurantDto::fromEntity)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        // .orElseThrow() 메서드의 경우 서버에러를 보내게 되는데 괄호 안에 어떤 오류는 던질건지 정할 수 있음
     }
 
     public RestaurantDto update(Long id, RestaurantDto dto) {
-        throw new RuntimeException("not implemented");
+        Optional<Restaurant> optionalRestaurant = repository.findById(id);
+        if (optionalRestaurant.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        Restaurant targetRestaurant = optionalRestaurant.get();
+        targetRestaurant.setName(dto.getName());
+        targetRestaurant.setCategory(dto.getCategory());
+        targetRestaurant.setOpenHours(dto.getOpenHour());
+        targetRestaurant.setCloseHours(dto.getCloseHour());
+        return RestaurantDto.fromEntity(repository.save(targetRestaurant));
     }
 
     public void delete(Long id) {
-        throw new RuntimeException("not implemented");
+        if (!repository.existsById(id))
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        repository.deleteById(id);
     }
 }
