0aa3e1b0ec6f1be7f15bb4e802e3de1838122aeb
HyeonJun222 <151013766+HyeonJun222@users.noreply.github.com>
2024-01-19 10:50:28 +0900

    Menu CRUD

--- src/main/java/com/example/restaurant/MenuService.java
+++ src/main/java/com/example/restaurant/MenuService.java
@@ -1,6 +1,7 @@
 package com.example.restaurant;
 
 import com.example.restaurant.dto.MenuDto;
+import com.example.restaurant.dto.RestaurantDto;
 import com.example.restaurant.entity.Menu;
 import com.example.restaurant.entity.Restaurant;
 import lombok.RequiredArgsConstructor;
@@ -21,22 +22,71 @@ public class MenuService {
     private final MenuRepository menuRepository;
 
     public MenuDto create(Long restId, MenuDto dto) {
-        throw new RuntimeException("not implemented");
+        Optional<Restaurant> optionalRestaurant = restaurantRepository.findById(restId);
+        if (optionalRestaurant.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        else{
+            Menu newMenu = new Menu(
+                    dto.getName(),
+                    dto.getPrice(),
+                    optionalRestaurant.get()
+            );
+            return MenuDto.fromEntity(menuRepository.save(newMenu));
+        }
     }
 
     public List<MenuDto> readAll(Long restId) {
-        throw new RuntimeException("not implemented");
+        if (!menuRepository.existsById(restId))
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        List<MenuDto> menuDtoList = new ArrayList<>();
+        List<Menu> menus = menuRepository.findAllByRestaurantId(restId);
+        for (Menu entity: menus){
+            menuDtoList.add(MenuDto.fromEntity(entity));
+        }
+        return menuDtoList;
     }
 
     public MenuDto read(Long restId, Long menuId) {
-        throw new RuntimeException("not implemented");
+        // Optional 이용해서 Menu를 찾아줌
+        Optional<Menu> optionalMenu = menuRepository.findById(menuId);
+        if (optionalMenu.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        // 검증절차 이 식당에 메뉴가 맞나?
+        Menu menu = optionalMenu.get();
+        if (!menu.getRestaurant().getId().equals(restId))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        return MenuDto.fromEntity(menu);
     }
 
     public MenuDto update(Long restId, Long menuId, MenuDto dto) {
-        throw new RuntimeException("not implemented");
+        // Optional 이용해서 Menu를 찾아줌
+        Optional<Menu> optionalMenu = menuRepository.findById(menuId);
+        if (optionalMenu.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        // 검증절차 이 식당에 메뉴가 맞나?
+        Menu menu = optionalMenu.get();
+        if (!menu.getRestaurant().getId().equals(restId))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        menu.setName(dto.getName());
+        menu.setPrice(dto.getPrice());
+        return MenuDto.fromEntity(menuRepository.save(menu));
     }
 
     public void delete(Long restId, Long menuId) {
-        throw new RuntimeException("not implemented");
+        // Optional 이용해서 Menu를 찾아줌
+        Optional<Menu> optionalMenu = menuRepository.findById(menuId);
+        if (optionalMenu.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        // 검증절차 이 식당에 메뉴가 맞나?
+        Menu menu = optionalMenu.get();
+        if (!menu.getRestaurant().getId().equals(restId))
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        menuRepository.deleteById(menuId);
     }
 }
